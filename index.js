50ea4323a2685f658a337ef1ec5d3b127dbfaa7d
Remko Plantenga <sonata82@gmail.com>
2020-04-13 13:52:02 +0200

    Fixed request & response parameter names

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URL = process.env.REDIRECT_URL;
 const APP_URL = process.env.APP_URL;
 
-app.get('/', function(request, response) {
-  console.log(`GET / with ${request.query.code}`);
-  request(`https://untappd.com/oauth/authorize/?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&response_type=code&redirect_url=${REDIRECT_URL}&code=${request.query.code}`, { json: true }, (err, res, body) => {
+app.get('/', function(req, res) {
+  console.log(`GET / with ${req.query.code}`);
+  request(`https://untappd.com/oauth/authorize/?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&response_type=code&redirect_url=${REDIRECT_URL}&code=${req.query.code}`, { json: true }, (err, response, body) => {
     if (err) { return console.log(err); }
     console.log(`Received access token ${body.response.access_token}`)
-    response.redirect(`${APP_URL}?access_token=${body.response.access_token}`)
+    res.redirect(`${APP_URL}?access_token=${body.response.access_token}`)
   });
 });
 
